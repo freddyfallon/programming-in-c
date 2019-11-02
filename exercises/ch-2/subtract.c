1b3cde52ca2184989579b580a7f8951eb659e2f4
Freddy Fallon <freddyfallon@gmail.com>
2019-11-02 14:35:16 +0000

    Add data types and arithmetic

---
+++ exercises/ch-2/subtract.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int val1;
+  int val2;
+  int result;
+
+  val1 = 87;
+  val2 = 15;
+  result = val1 - val2;
+
+  printf("%i minus %i is %i", val1, val2, result);
+
+  return 0;
+}
---
+++ exercises/ch-2/sum.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int sum;
+
+  // compute result
+  sum = 25 + 37 + 18;
+
+  // display results
+  printf("The answer is %i", sum);
+
+  return 0;
+}
